e3dfcda2092c30b4c0a790e7de16c607b25bdfd7
Daniele Muffato <daniele.muffato@studio.unibo.it>
2022-11-09 21:06:26 +0100

    61-anonymous-nested-enum DONE!

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,8 +1,6 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -11,11 +9,66 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String arg0, String arg1) {
+                final var m1 = Month.fromString(arg0);
+                final var m2 = Month.fromString(arg1);
+                return Integer.compare(m1.days, m2.days);
+            }
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String arg0, String arg1) {
+                return Month.fromString(arg0).compareTo(Month.fromString(arg1));
+            }
+        };
+    }
+
+    private enum Month {
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        private Month(final int days) {
+            this.days = days;
+        }
+
+        public static Month fromString(final String name) {
+            try {
+                return valueOf(name);
+            } catch (IllegalArgumentException e) {
+                Month match = null;
+                for (Month month : values()) {
+                    if (month.toString().toLowerCase().startsWith(name.toLowerCase())) {
+                        if (match == null) {
+                            match = month;
+                        } else {
+                            throw new IllegalArgumentException();
+                        }
+                    }
+                }
+                if (match == null) {
+                    throw new IllegalArgumentException();
+                } else {
+                    return match;
+                }
+            }
+        }
     }
 }
